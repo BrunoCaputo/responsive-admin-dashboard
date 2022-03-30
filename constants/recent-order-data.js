4ed6c035343b94b6961901ce7054f01d4a1eb75f
BrunoCaputo <brunopc.dev@gmail.com>
2022-03-29 21:21:40 -0300

    feat: Added order table with JS body builder

---
+++ constants/recent-order-data.js
@@ -0,0 +1,9 @@
+const RECENT_ORDER_DATA = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    payment: "Due",
+    status: "Pending",
+    statusColor: "warning",
+  },
+];
---
+++ index.js
@@ -0,0 +1,27 @@
+// Executes when document is loaded
+document.addEventListener("DOMContentLoaded", (ev) => {
+  document.getElementById("recent-order--table").appendChild(buildTableBody());
+});
+
+const buildTableBody = () => {
+  const recentOrderData = RECENT_ORDER_DATA;
+
+  const tbody = document.createElement("tbody");
+
+  let bodyContent = "";
+  for (const row of recentOrderData) {
+    bodyContent += `
+      <tr>
+        <td>${row.productName}</td>
+        <td>${row.productNumber}</td>
+        <td>${row.payment}</td>
+        <td class="${row.statusColor}">${row.status}</td>
+        <td class="primary">Details</td>
+      </tr>
+    `;
+  }
+
+  tbody.innerHTML = bodyContent;
+
+  return tbody;
+};
