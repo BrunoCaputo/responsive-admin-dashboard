d379011a283fcb1dc4b7514a9d693daca091d6a5
BrunoCaputo <brunopc.dev@gmail.com>
2022-03-30 23:31:34 -0300

    feat: Added dark theme toggler

--- index.js
+++ index.js
@@ -96,11 +96,22 @@ const buildSalesAnalytics = (element) => {
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
+// Show Sidebar
 menuBtn.addEventListener("click", () => {
   sideMenu.style.display = "block";
 });
 
+// Hide Sidebar
 closeBtn.addEventListener("click", () => {
   sideMenu.style.display = "none";
+});
+
+// Change Theme
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
