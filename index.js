738ae031d549a45c607bc3a744dbe5f205373b43
BrunoCaputo <brunopc.dev@gmail.com>
2022-03-30 23:15:18 -0300

    feat: Added show and hide menu function

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", (ev) => {
   buildSalesAnalytics(salesAnalytics);
 });
 
+// Document Builder
 const buildTableBody = () => {
   const recentOrderData = RECENT_ORDER_DATA;
 
@@ -90,3 +91,16 @@ const buildSalesAnalytics = (element) => {
     element.appendChild(item);
   }
 };
+
+// Document operation functions
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
